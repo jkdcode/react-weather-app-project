5e58296c00d4387559516098dd7378dfd6671d59
jkdcode <jk.didriksen@gmail.com>
2024-04-19 18:22:50 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
+import "bootstrap/dist/css/bootstrap.css";
+
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        <a
-          href="https://julie-makes-stuff.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Julie Makes Stuff
-        </a>{" "}
-        |{" "}
-        <a
-          href="https://github.com/jkdcode/react-weather-app-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>{" "}
-        |{" "}
-        <a
-          href="https://julies-react-weather-app.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Netlify
-        </a>
-      </footer>
+      <div className="Container">
+        <h1>Weather app</h1>
+        <footer>
+          <a
+            href="https://julie-makes-stuff.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Julie Makes Stuff
+          </a>{" "}
+          |{" "}
+          <a
+            href="https://github.com/jkdcode/react-weather-app-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          |{" "}
+          <a
+            href="https://julies-react-weather-app.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
