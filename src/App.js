132408bf2b64da67fdc2b5a2b271f87d695d084d
jkdcode <jk.didriksen@gmail.com>
2024-04-19 17:44:49 +0200

    Added footer with links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <footer>
+        <a
+          href="https://julie-makes-stuff.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Julie Makes Stuff
+        </a>{" "}
+        |{" "}
+        <a
+          href="https://github.com/jkdcode/react-weather-app-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>{" "}
+        |{" "}
+        <a
+          href="https://julies-react-weather-app.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
+    </div>
+  );
 }
