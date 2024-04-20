13f7d17b0f67bd2fd2aeca373ae0e7fe94923101
jkdcode <jk.didriksen@gmail.com>
2024-04-20 19:34:08 +0200

    weather icons now based on live data

--- src/Weather.js
+++ src/Weather.js
@@ -24,7 +24,7 @@ export default function Weather(props) {
         temperature: response.data.main.temp,
         wind: response.data.wind.speed,
         humidity: response.data.main.humidity,
-        iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+        iconUrl: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
         date: new Date(response.data.dt * 1000),
         feelsLike: response.data.main.feels_like,
       });
